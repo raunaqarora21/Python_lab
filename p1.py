ea92827dde936b90afd5e4d87fc8c2507e8d043d
raunaqarora21 <60340725+raunaqarora21@users.noreply.github.com>
2020-02-12 11:37:16 +0530

    Add files via upload
    Set

--- p1.py
+++ p1.py
@@ -1,10 +1,9 @@
-a=list()
-while 1:
-	item=input("Enter the item ")
-	a.append(item)
-	n=input("Do you want to continue Y/N ")
-	if n.lower() == 'n':
-		break
-a.sort(reverse=True,key=max)
+l1=[1,3,4,2]
+l2=[1,4,9,5]
+l3=[i for i in l1 if i not in l2]
+l4=[i for i in l2 if i not in l1]
+a=l3+l4
+print(a)
 
-print(a[0])
+
+
