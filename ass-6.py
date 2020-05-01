ee7adea8a93eeb2774f20982685fbc2087156a23
Raunaq Arora <60340725+raunaqarora21@users.noreply.github.com>
2020-05-01 19:18:04 +0530

    Bottle Necks

---
+++ ass-6.py
@@ -0,0 +1,26 @@
+n=int(input())
+l=list(map(int,input().split()))
+
+
+x=0
+while(x<(n-1)):
+     if l[x]<l[x+1]:
+         l.remove(l[x])
+         n-=1
+         
+     else:
+         x+=1
+
+y=0
+while(y<(n-1)):
+    if l[y]<l[y+1]:
+         l.remove(l[y])
+         n-=1
+        
+    else:
+         y+=1
+         
+if l[-1]<l[-2]:
+    l.remove(l[-1])
+
+print(len(l))
